dbcc8c8e790bee5b949eaeb96af61644fe81a211
Jeremiah Currier <jcurrier@slack-corp.com>
2019-01-06 12:30:23 -0800

    Add tests for GET /todos

--- server/server.js
+++ server/server.js
@@ -9,12 +9,6 @@ var app = express();
 
 app.use(bodyParser.json());
 
-// expect = assertions
-// mocha = entire test suite
-// supertest = to test express routes
-// nodemon = lets us create test-watch script we had so we can automatically restart the test suite - nodemon is installed globally but since using inside a package.json script = good idea to install locally as well
-// 1 - verify if send correct data as body we get back 200 w completed doc including the id
-// 2 - if we send bad data expect 400 with error obj
 app.post('/todos', (req, res) => {
 	var todo = new Todo({
 		text: req.body.text
@@ -23,6 +17,15 @@ app.post('/todos', (req, res) => {
 	todo.save().then((doc) => {
 		res.send(doc);
 	}, (e) => {
+		res.status(400).send(e);
+	});
+});
+
+app.get('/todos', (req, res) => {
+	Todo.find().then((todos) => {
+		res.send({todos}); // create an object so you can add properties later on VS sending an Array back. More flexible future
+	}, (e) => {
+		// e
 		res.status(400).send(e);
 	});
 });
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -5,10 +5,24 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+
+// ADD SEED DATA
+// dummy todos
+const todos = [{
+  text: 'First test todo'
+}, {
+  text: 'Second test todo'
+}];
+
+
 // add TESTING LIFECYCLE METHOD
 beforeEach((done) => {
   // run before every test case
-  Todo.remove({}).then(() => done()); // wipe out our todos
+  // Todo.remove({}).then(() => done()); // wipe out our todos
+  // insertMany (takes array and inserts into collection)
+  Todo.remove({}).then(() => {
+    return Todo.insertMany(todos);
+  }).then(() => done());
 });
 
 // group all routes
@@ -30,7 +44,7 @@ describe('POST /todos', () => {
         }
 
         // request to db fetch todos verifying one was added
-        Todo.find().then((todos) => {
+        Todo.find({text}).then((todos) => {
           expect(todos.length).toBe(1);
           expect(todos[0].text).toBe(text);
           done();
@@ -50,10 +64,22 @@ describe('POST /todos', () => {
         }
 
         Todo.find().then((todos) => {
-          expect(todos.length).toBe(0);
+          expect(todos.length).toBe(2);
           done();
         }).catch((e) => done(e));
       });
   })
 
+});
+
+describe('GET /todos', () => {
+  it('should get all todos', (done) => {
+    request(app)
+      .get('/todos')
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todos.length).toBe(2);
+      })
+      .end(done);
+  });
 });
